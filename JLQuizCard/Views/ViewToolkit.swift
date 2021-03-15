3d5c440668f289863675144f45e8f2a07c862646
jacklandrin <jacklandrin@hotmail.com>
2021-03-16 00:45:55 +0100

    add view toolkit

---
+++ JLQuizCard/Views/ViewToolkit.swift
@@ -0,0 +1,28 @@
+//
+//  ViewToolkit.swift
+//  JLQuizCard
+//
+//  Created by jack on 2021/3/16.
+//  Copyright © 2021 jack. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
